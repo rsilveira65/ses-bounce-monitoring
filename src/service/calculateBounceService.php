08d5c3fdebc3a38efcf9236f1127b6ccd072d466
Rafael Silveira <rafael.silveira@possible.com>
2017-10-25 13:22:32 -0200

    :bug: - fix max bounce

--- src/service/calculateBounceService.php
+++ src/service/calculateBounceService.php
@@ -17,18 +17,18 @@ class calculateBounceService
      */
     static function getBounce($result)
     {
-        $totalBounces = 0;
+        $totalBounces[] = 0;
 
         foreach ($result['SendDataPoints'] as $sendDataPoint) {
 
             if (self::hasStatisticsToday($sendDataPoint)) {
                 if ($sendDataPoint['Bounces'] > 0) {
-                    $totalBounces += intval($sendDataPoint['Bounces']);
+                    $totalBounces[] = intval($sendDataPoint['Bounces']);
                 }
             }
         }
 
-        return $totalBounces;
+        return max($totalBounces);
     }
 
     /**
