fe7692bf25785d215666efd3f8f9752fb99f5549
rsilveira65 <rsilveiracc@gmail.com>
2017-11-22 22:16:17 -0200

    fix bounce counter

--- src/service/calculateBounceService.php
+++ src/service/calculateBounceService.php
@@ -16,17 +16,17 @@ class calculateBounceService
      */
     static function getBounce($result)
     {
-        $totalBounces[] = 0;
+        $totalBounces = 0;
 
         foreach ($result['SendDataPoints'] as $sendDataPoint) {
             if (self::hasStatisticsToday($sendDataPoint)) {
                 if ($sendDataPoint['Bounces'] > 0) {
-                    $totalBounces[] = intval($sendDataPoint['Bounces']);
+                    $totalBounces += intval($sendDataPoint['Bounces']);
                 }
             }
         }
 
-        return max($totalBounces);
+        return $totalBounces;
     }
 
     /**
